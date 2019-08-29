494ce271f387bc41e4d767a5aa921048ebf48c95
Ralf Hortt <me@horttcore.de>
2019-08-29 21:44:11 +0200

    Initial release

---
+++ wp-maybe-remove-comments.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Maybe disable comments
+ */
+namespace RalfHortt\MaybeDisableComments;
+
+$commentsClosed = get_option('default_comment_status') == 'closed' ? true : false;
+$commentCount = $wpdb->get_var("SELECT COUNT(comment_ID) FROM $wpdb->comments");
+
+if (!$commentsClosed || $commentCount > 0) {
+    return;
+}
+
+add_action('admin_menu', function () {
+    remove_menu_page('edit-comments.php'); // Kommentare
+});
