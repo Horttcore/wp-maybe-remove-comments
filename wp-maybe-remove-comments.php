5f92337794794aa97cb82cbf2ff53593a77c7ddb
Ralf Hortt <me@horttcore.de>
2025-06-25 09:49:26 +0200

    Update wp-maybe-remove-comments.php
    Remove from admin bar

--- wp-maybe-remove-comments.php
+++ wp-maybe-remove-comments.php
@@ -3,7 +3,7 @@
 Plugin Name:  WP Maybe Remove Comments
 Plugin URI:   https://github.com/Horttcore/wp-maybe-remove-comments
 Description:  Maybe disable comments admin menu entry.
-Version:      1.3.0
+Version:      1.4.0
 Author:       Ralf Hortt
 Author URI:   https://horttcore.de/
 License:      GPL
@@ -12,6 +12,12 @@ License:      GPL
 namespace RalfHortt\MaybeDisableComments;
 
 global $wpdb;
+
+// Do nothing if not logged in
+if (!is_user_logged_in()) {
+    return
+}
+
 $commentStatus = get_option('default_comment_status');
 $commentsClosed = $commentStatus == 'closed' || !$commentStatus ? true : false;
 $commentCount = $wpdb->get_var("SELECT COUNT(comment_ID) FROM $wpdb->comments WHERE comment_type = ''");
@@ -20,6 +26,13 @@ if (!$commentsClosed || $commentCount > 0) {
     return;
 }
 
+// Remove menu entry
 add_action('admin_menu', function () {
     remove_menu_page('edit-comments.php'); // Kommentare
 });
+
+// Remove from admin bar
+add_action('wp_before_admin_bar_render', function () {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('comments');
+})
